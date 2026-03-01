52f99949272817bf2e03ef97882cb4eaa167e771
Wolf Nederpel <wolf.nederpel@vortech.nl>
2026-03-01 21:18:09 +0100

    Remove print output in non-debug mode and turn of some functionality for first proper version

--- scripts/arenant.jl
+++ scripts/arenant.jl
@@ -1,8 +1,8 @@
 using Intsect
 
 time_limit = 0.1
-debug = false
-full_debug = false
+debug = true
+full_debug = true
 Arenant.run_arena(; debug=debug, time_limit_s=time_limit, full_debug=full_debug)
 # Arenant.play_one_match(
 #     "engines\\source",
@@ -11,4 +11,4 @@ Arenant.run_arena(; debug=debug, time_limit_s=time_limit, full_debug=full_debug)
 #     time_limit;
 #     starting_position=raw"wL;bL wL\;wA1 \wL;bM bL\;wQ /wA1;bA1 /bL;wA1 bM-;bQ bA1\\",
 #     debug=debug,
-)
+# )
